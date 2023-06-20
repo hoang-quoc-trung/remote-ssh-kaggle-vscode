8e5e4df0c63a2b34091b54ee35c96fec2b2dfe0c
Trung Hoang <hoangtrung020541@gmail.com>
2023-06-20 16:17:10 +0700

    feat: add bash script to install cuda 11.8

---
+++ install_cuda.sh
@@ -0,0 +1,13 @@
+rm -r cuda
+mkdir cuda
+cd cuda
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+sudo rm /bin/nvidia-smi
+sudo ln -s /opt/bin/nvidia-smi /bin/
+mamba init
